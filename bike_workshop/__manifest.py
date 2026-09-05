1bba3154fe9930ed5a9a7d9e9519dd2fa2142741
Shahed2463 <shahedmahmoud303@gmail.com>
2026-09-05 20:53:35 +0300

    Add Exercise 1: Rami's Bike Workshop module

---
+++ bike_workshop/__manifest.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Bike Workshop',
+    'version': '19.0.1.0.0',
+    'summary': 'Manage bikes and rental operations for Rami\'s Workshop',
+    'category': 'Services',
+    'author': 'Shahed Alawneh',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/bike_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
